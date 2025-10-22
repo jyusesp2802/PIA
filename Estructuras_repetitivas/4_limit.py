29e8bbf625b49e2afbdd7eaac8769cac23514f18
jaimeyust <jaimeyust8@gmail.com>
2025-10-22 13:14:11 +0200

    Practicas

--- Estructuras_repetitivas/4_limit.py
+++ Estructuras_repetitivas/4_limit.py
@@ -13,19 +13,39 @@ Fecha: 16/10/2025
 """
 print('Este programa pide un intervalo y luego números para analizar su posición respecto al intervalo.')
 
-lower_range = int(input("Introduce el límite inferior del intervalo: "))
-upper_range = int(input("Introduce el límite superior del intervalo: "))
-
 sum_range = 0
 out_range_count = 0
 equal_limit = False
+
+lower_range = int(input("Introduce el límite inferior del intervalo: "))
+upper_range = int(input("Introduce el límite superior del intervalo: "))
+
 
 while lower_range >= upper_range:
     print("Error: El límite inferior debe ser menor que el límite superior.")
     lower_range = int(input("Introduce el límite inferior del intervalo: "))
     upper_range = int(input("Introduce el límite superior del intervalo: "))
-else:
 
+num = int(input("\nIntroduce un número (0, para salir): "))
+
+while num != 0:
+    if lower_range < num < upper_range:  # Pertenece al intervalo (num>lim_inf and num<lim_sup en otro lenguaje)
+        sum_range += num
+    else:  # No pertenece al intervalo
+        out_range_count += 1
+        # Número igual a alguno de los límites
+        if num == lower_range or num == upper_range:
+            equal_limit = True
+    num = int(input("Introduce un número (0, para salir): "))
+
+# Resultados
+print("\nRESULTADOS:")
+print(f"La suma de los números dentro del intervalo es {sum_range}")
+print(f"La cantidad de números fuera del intervalo es {out_range_count}")
+if equal_limit:
+    print("Se ha introducido algún número igual a los límites del intervalo.")
+else:
+    print("No se ha introducido ningún número igual a los límites del intervalo.")
 
 
 
