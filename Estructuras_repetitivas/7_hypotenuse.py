79736568a15c66e9ab5d994d572c5e7932b8fe2c
jaimeyust <jaimeyust8@gmail.com>
2025-11-05 20:33:19 +0100

    Practicas

--- Estructuras_repetitivas/7_hypotenuse.py
+++ Estructuras_repetitivas/7_hypotenuse.py
@@ -20,19 +20,7 @@ tasa_interes_mensual = tasa_interes_anual / 100 / 12 #Convertir la tasa anual a 
 numero_pagos = plazo_anos * 12 #Número total de pagos mensuales
 cuota_mensual = (importe_prestamo * tasa_interes_mensual) / (1 - (1 + tasa_interes_mensual) ** -numero_pagos) #Fórmula de la cuota mensual
 
-print(f"\nLa cuota mensual de la hipoteca es: {cuota_mensual:.2f}€\n")
-print("Tabla de amortización:")
-print("Pago\tInterés Principal\tSaldo Restante\t")
 
-while importe_prestamo > 0:
-    interes_mensual = importe_prestamo * tasa_interes_mensual
-    principal_mensual = cuota_mensual - interes_mensual
-    importe_prestamo -= principal_mensual
-    if importe_prestamo < 0:
-        principal_mensual += importe_prestamo
-        importe_prestamo = 0
-    print(f"{numero_pagos}\t{interes_mensual:.2f}\t\t{principal_mensual:.2f}\t\t{importe_prestamo:.2f}")
-    numero_pagos -= 1
 
 
 
