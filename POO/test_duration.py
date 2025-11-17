a071e4c712cac6ae8a7b25a109801934578240bd
jaimeyust <jaimeyust8@gmail.com>
2025-11-17 16:15:02 +0100

    Practicas

--- POO/test_duration.py
+++ POO/test_duration.py
@@ -1,13 +1,13 @@
-from fractions import Fraction
+from duration import Duration
 
-d1 = Fraction(4, 3)
-d2 = Fraction(1, -2)
+d1 = Duration(4, 3)
+d2 = Duration(1, -2)
 
 print(f'd1 = {d1}')
 print(f'd2 = {d2}')
 
 d3 = d1 + d2
-d4 = d1 * d2
+d4 = d1 - d2
 
 print(f'd3 = {d3}')
 print(f'd4 = {d4}')
